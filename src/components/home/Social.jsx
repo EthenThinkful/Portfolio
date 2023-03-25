5ed320dd782b53c060824ec8e97dc285839a59be
Ethen Roth <Ethenfm@gmail.com>
2023-03-25 17:32:13 -0600

    initial upload

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/ethen-roth/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+
+      <a
+        href="https://github.com/EthenThinkful"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
