897083b6cf84e32089eb1d97e0214c4eb5df82d3
Ethen Roth <Ethenfm@gmail.com>
2023-03-26 17:40:30 -0600

    developing services section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header/Header';
 import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Header />
       <div className='main'>
         <Home />
+        <About />
+        <Skills />
+        <Services />
       </div>
     </>
   );
