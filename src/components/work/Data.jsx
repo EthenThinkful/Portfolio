16e7181fae0a8e8a2d46eab51feafd955aa46ee4
Ethen Roth <Ethenfm@gmail.com>
2023-05-25 13:51:38 -0600

    updated projects with github links and added new clinicals project

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,7 +1,8 @@
 import Work1 from "../../assets/assets/restaurantReservations.png";
-import Work2 from "../../assets/assets/flashCardApp.png";
+import Work2 from "../../assets/assets/clinicals.PNG";
 import Work3 from "../../assets/assets/weLoveMovies.png";
 import Work4 from "../../assets/assets/grubDash.png";
+import Work5 from "../../assets/assets/flashCardApp.png";
 
 export const projectsData = [
     {
@@ -9,28 +10,39 @@ export const projectsData = [
         image: Work1,
         title: 'Restaurant reservations',
         category: 'fullstack',
-        url: 'https://periodictables.vercel.app/dashboard'
+        url: 'https://periodictables.vercel.app/dashboard',
+        url2: 'https://github.com/EthenThinkful/Restaurant-Reservation-App'
     },
     {
         id: 2,
         image: Work2,
-        title: 'Study card application',
-        category: 'frontend',
-        url: 'https://github.com/EthenThinkful/FlashCardApp'
+        title: 'Clinical Data Logger',
+        category: 'fullstack',
+        url: 'http://clinicaldataloggerfe.s3-website-us-east-1.amazonaws.com/',
+        url2: 'https://github.com/EthenThinkful/ClinicalDataLoggerBackend'
     },
     {
         id: 3,
         image: Work3,
         title: 'Movie reviews',
         category: 'backend',
-        url: 'https://we-love-movies-frontend-red.vercel.app/'
+        url: 'https://we-love-movies-frontend-red.vercel.app/',
+        url2: 'https://github.com/EthenThinkful/WeLoveMovies'
     },
     {
         id: 4,
         image: Work4,
         title: 'GrubDash',
-        category: 'fullstack',
-        url: 'https://grub-dash-front-end-tawny.vercel.app/'
+        category: 'backend',
+        url: 'https://grub-dash-front-end-tawny.vercel.app/',
+        url2: 'https://github.com/EthenThinkful/GrubDash'
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: 'Study card application',
+        category: 'frontend',
+        url2: 'https://github.com/EthenThinkful/FlashCardApp'
     }
 ]
 
--- src/components/work/WorksItems.jsx
+++ src/components/work/WorksItems.jsx
@@ -5,10 +5,21 @@ const WorksItems = ({item}) => {
         <div className="work__card" key={item.id}>
             <img src={item.image} alt="" className="work__img"/>
             <h3 className="work__title">{item.title}</h3>
+            {item.id === 5 ? <a href={item.url2} className="work__button">
+                Github <i className="bx bx-right-arrow-alt work__button-icon">
+            </i>
+            </a> :
+            <div className="link__container">
             <a href={item.url} className="work__button">
-                Demo <i className="bx bx-right-arrow-alt work__button-icon">
-                </i>
-            </a>
+            Demo <i className="bx bx-right-arrow-alt work__button-icon">
+            </i>
+        </a>
+        <a href={item.url2} className="work__button">
+            Github <i className="uil uil-github work__button-icon"></i>
+        </a>
+        </div>
+        
+        }
         </div>
     )
 }
