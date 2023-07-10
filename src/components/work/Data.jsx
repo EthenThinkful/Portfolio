97d0823817bcf45e998e7b9d68b57d9a2b2facbb
Ethen Roth <Ethenfm@gmail.com>
2023-07-10 15:10:07 -0600

    added descriptions to projects

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -12,7 +12,8 @@ export const projectsData = [
         title: 'Restaurant Reservations',
         category: 'fullstack',
         url: 'https://periodictables.vercel.app/dashboard',
-        url2: 'https://github.com/EthenThinkful/Restaurant-Reservation-App'
+        url2: 'https://github.com/EthenThinkful/Restaurant-Reservation-App', 
+        description: 'PERN Stack Application developed for restaurant personnel to keep track of reservations and seating.'
     },
     {
         id: 2,
@@ -20,7 +21,8 @@ export const projectsData = [
         title: 'Arbibabes (NFT Promo Site)',
         category: 'frontend',
         url: 'https://optichads.vercel.app/babes',
-        url2: 'https://github.com/DistractionBoy/optichads'
+        url2: 'https://github.com/DistractionBoy/optichads',
+        description: 'Learned Typescript & Next.js, an advanced react based framework, by contributing to frontend tickets per the development team.'
     },
     {
         id: 3,
@@ -28,7 +30,8 @@ export const projectsData = [
         title: 'Movie Reviews',
         category: 'backend',
         url: 'https://we-love-movies-frontend-red.vercel.app/',
-        url2: 'https://github.com/EthenThinkful/WeLoveMovies'
+        url2: 'https://github.com/EthenThinkful/WeLoveMovies',
+        description: "Created to demonstrate my understanding of backend code and RESTful principles per Thinkful's cirriculum."
     },
     {
         id: 4,
@@ -36,14 +39,16 @@ export const projectsData = [
         title: 'Clinical Data Logger',
         category: 'fullstack',
         url: 'http://clinicaldataloggerfe.s3-website-us-east-1.amazonaws.com/',
-        url2: 'https://github.com/EthenThinkful/ClinicalDataLoggerBackend'
+        url2: 'https://github.com/EthenThinkful/ClinicalDataLoggerBackend',
+        description: 'After learning Java & Spring, decided to make a simplistic full stack app utilizing a Spring built API, React, & AWS.'
     },
     {
         id: 5,
         image: Work5,
         title: 'Study Card Application',
         category: 'frontend',
-        url2: 'https://github.com/EthenThinkful/FlashCardApp'
+        url2: 'https://github.com/EthenThinkful/FlashCardApp', 
+        description: 'Flashcard project that got me out of the React learning curve by using a plethora of React hooks.'
     },
     {
         id: 6, 
@@ -51,7 +56,8 @@ export const projectsData = [
         title: 'GrubDash',
         category: 'backend',
         url: 'https://grub-dash-front-end-tawny.vercel.app/',
-        url2: 'https://github.com/EthenThinkful/GrubDash'
+        url2: 'https://github.com/EthenThinkful/GrubDash', 
+        description: "This project tested my ability to build APIs with complex validation, per Thinkful's cirriculum, & connecting to a frontend."
     }
 ]
 
--- src/components/work/WorksItems.jsx
+++ src/components/work/WorksItems.jsx
@@ -5,6 +5,7 @@ const WorksItems = ({item}) => {
         <div className="work__card" key={item.id}>
             <img src={item.image} alt="" className="work__img"/>
             <h3 className="work__title">{item.title}</h3>
+            <div className="work__desc">{item.description}</div>
             {item.id === 5 ? <a href={item.url2} className="work__button">
                 Github <i className="bx bx-right-arrow-alt work__button-icon">
             </i>
